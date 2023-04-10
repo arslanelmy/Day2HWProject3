a1d4e548e63d4d9435fcd992296d005d1f71ee25
Phoenix <Phoenix@10.0.0.221>
2023-04-10 17:27:17 -0400

    commit stash

--- src/test/demo3.java
+++ src/test/demo3.java
@@ -2,4 +2,10 @@ package test;
 
 public class demo3 {
 
+	public static void main(String[] args) {
+		System.out.println("demo 3...");
+	}
+	public void multiply() {
+		System.out.println(2*2);
+	}
 }
